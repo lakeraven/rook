9512881b4e918eba7d1a1d6793934c5c72d59320
Kimball Bighorse <kbighorse@yahoo.com>
2026-08-16 01:09:02 -0700

    Genericize example tenant identifiers in demo tests
    Replace real partner name in the leak-guard assertions with a neutral
    placeholder so no real tribe name appears in the public repo.

--- test/demo/gpra_report_test.rb
+++ test/demo/gpra_report_test.rb
@@ -81,7 +81,7 @@ class Rook::Demo::GPRAReportTest < Minitest::Test
     assert_includes html, "<!DOCTYPE html>"
     assert_includes html, "IHS CRS / GPRA National Clinical Measures"
     assert_includes html, "80.0%"
-    refute_match(/good medicine|mcuih|yakama/i, html)
+    refute_match(/good medicine|mcuih|example tribe/i, html)
   end
 
   private
